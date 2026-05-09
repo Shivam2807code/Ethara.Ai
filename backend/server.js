2340d94ab5dd30aa2c634515d617fcfb34dd5b2c
shivamworks28-web <your_email@example.com>
2026-05-09 16:46:03 +0530

    Changes Made

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
@@ -11,14 +14,26 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.json({ message: "Team Task Manager API running" }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 5001 ;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
